125f94ca847531db111ddd16e56cf64a29159cf4
hkumarmk <hkumarmk@gmail.com>
2015-02-20 00:26:40 +0530

    Consul based leader election
    * jiocloud::utils changed to avoid code failures especially for facts as fact
     failure can cause puppet resources never get executed.
    * Changed consul_session resource to use jiocloud::utils, simplified it.
    * created a facter to generate facts - leader, and cluster_role
      leader: who is the leader
      cluster_role: what is current node's role in the cluster - leader or follower

--- lib/jiocloud/utils.rb
+++ lib/jiocloud/utils.rb
@@ -94,7 +94,7 @@ module Jiocloud::Utils
   # Check-and-Set for session.
   ##
   def casSession(name,args={})
-    if getSessionID(name) == ''
+    if getSessionID({:name => name}) == ''
       createSession(name,args)
     else
       true
@@ -104,9 +104,9 @@ module Jiocloud::Utils
   def createSession(name,args={})
     body_hash = {}
     body_hash['Name'] = name
-    body_hash['LockDelay'] = args['lockdelay'] if args.key?('lockdelay')
-    body_hash['Node'] = args['node'] if args.key?('node')
-    body_hash['Checks'] = args['node'] if args.key?('checks')
+    body_hash['LockDelay'] = args[:lockdelay] if args.key?(:lockdelay)
+    body_hash['Node'] = args[:node] if args.key?(:node)
+    body_hash['Checks'] = args[:checks] if args.key?(:checks)
     body = body_hash.to_json
     data = put(sessionurl + '/create',body)
     if data.nil?
@@ -120,34 +120,54 @@ module Jiocloud::Utils
     end
   end
 
-  def getSessionID(name)
-    return getSession(name)['ID']
+  def getSessionID(args={})
+    @sessionID ||= getSession(args)['ID']
+    return @sessionID
   end
 
-  def getSession(name)
-    sessions = get(sessionurl + '/list')
-    if sessions.nil?
-      return nil
+  def getSessionName(args={})
+    return getSession(args)['Name']
+  end
+
+  ##
+  #
+  ##
+  def getSession(args = {})
+    if args.key?(:id) && ! args[:id].nil?
+      session = Jiocloud::Utils.get(sessionurl + '/info/' + args[:id])
+      if session.nil?                                             
+        return nil  
+      end
+    elsif args.key?(:name) && ! args[:name].nil?
+      if args.key?(:node) && ! args[:node].nil?
+        sessions = Jiocloud::Utils.get(sessionurl + '/node/' + args[:node])
+      else
+        sessions = Jiocloud::Utils.get(sessionurl + '/list')
+      end
+      if sessions.nil?
+        return nil
+      end
+      session = sessions.select {|session| session['Name'] == args[:name]}
     end
-    session = sessions.select {|session| session['Name'] == name}
+
     if session.empty?
       return ''
     elsif session.count > 1
-      raise(Puppet::Error,"Multiple matching (#{session.count}) Consul Sessions found for #{name}")
+      raise(Puppet::Error,"Multiple matching (#{session.count}) Consul Sessions found for #{args[:name]}")
     else
       return session[0]
     end
   end
 
-  def deleteSession(name)
-    put(sessionurl + '/destroy/' + getSessionID(name),'')
+  def deleteSession(name,node=nil)
+    Jiocloud::Utils.put(sessionurl + '/destroy/' + getSessionID({:name => name,:node => node}),'')
   end
 
   ##
   # return the session belonging to specified node
   ##
-  def getNodeSession(node)
-    sessions = get(sessionurl + '/node/' + node)
+  def getNodeSessions(node)
+    sessions = Jiocloud::Utils.get(sessionurl + '/node/' + node)
     if sessions.nil?
       return nil
     end
@@ -155,36 +175,25 @@ module Jiocloud::Utils
   end
 
   ##
-  # Get session blonging to specified node with specifiied name
+  # Return an array of session names belonging to the specified node
   ##
-  def getNodeSessionWithName(node,name)
-    sessions = get(sessionurl + '/node/' + node)
+  def getNodeSessionNames(node)
+    sessions = getNodeSessions(node)
     if sessions.nil?
       return nil
     end
-    session = sessions.select {|session| session['Name'] == name}
-    if session.empty?
-      return ''
-    elsif session.count > 1
-      raise(Puppet::Error,"Multiple matching (#{session.count}) Consul Sessions found for #{node, #{name}")
-    else
-      return session[0]
-    end
-  end
-
-
-  ##
-  # Return an array of session names belonging to the specified node
-  ##
-  def getNodeSessionName(node)
-    getNodeSession(node).inject([]){ |r,x| r << x.values_at('Name') }.flatten
+    return sessions.inject([]){ |r,x| r << x.values_at('Name') }.flatten
   end
 
   ##
   # Return array of session ids belonging to the specified node
   ##
-  def getNodeSessionID(node)
-    getNodeSession(node).inject([]){ |r,x| r << x.values_at('ID') }.flatten
+  def getNodeSessionIDs(node)
+    sessions = getNodeSessions(node)
+    if sessions.nil?
+      return nil
+    end
+    return sessions.inject([]){ |r,x| r << x.values_at('ID') }.flatten
   end
 
   ##
@@ -194,37 +203,72 @@ module Jiocloud::Utils
 
   def createKV(key,value,args={})
     url_params = []
-    if args.key?('acquire')
-      session_name = args['acquire']
-      session_id = getSessionID(session_name)
+    if args.key?(:acquire) && ! args[:acquire].nil?
+      session_name = args[:acquire]
+      node         = args[:node]
+      session_id = getSessionID({:name => session_name, :node => node})
+      if session_id.nil?
+        return nil
+      end
       url_params << 'acquire=' + session_id
     end
 
-    if args.key?('release')
-      session_name = args['release']
-      session_id = getSessionID(session_name)
+    if args.key?(:release) && ! args[:release].nil?
+      session_name = args[:release]
+      node         = args[:node]
+      session_id = getSessionID({:name => session_name,:node => node})
+      if session_id.nil?
+        return nil
+      end
       url_params << 'release=' + session_id
     end
+
+    url_params << "flags=#{args[:flags]}" if args.key?(:flags) && ! args[:flags].nil?
+
+    url_params << "cas=#{args[:cas]}" if args.key?(:cas) && ! args[:cas].nil?
     if url_params.empty?
-      put(kvurl + '/' + key,value)
+      Jiocloud::Utils.put(kvurl + '/' + key,value)
     else
-      put(kvurl + '/' + key + '?' + url_params.join('&'),value)
+      Jiocloud::Utils.put(kvurl + '/' + key + '?' + url_params.join('&'),value)
     end
   end
 
+  def getLockSession(key)
+    return nil if key.nil?
+    kv = getKV(key) 
+    if kv.nil?     
+      return nil    
+    elsif kv.empty? 
+      return ''     
+    else
+      return kv['Session']
+    end
+  end
+
+  def getKvValue(key)
+    kv = getKV(key)
+    if key.nil?    
+      return nil   
+    elsif key.empty? 
+      return ''    
+    else           
+      return Base64.decode64(kv['Value'])
+    end 
+  end
+
   def getKV(key)
-    key = get(kvurl + '/' + key)
+    key = Jiocloud::Utils.get(kvurl + '/' + key)
     if key.nil?
       return nil
     elsif key.empty?
       return ''
     else
-      return Base64.decode64(key[0]['Value'])
+      return key[0]
     end
   end
 
   def deleteKV(key)
-    delete(kvurl + '/' + key)
+    Jiocloud::Utils.delete(kvurl + '/' + key)
   end
 
 end
--- lib/puppet/type/consul_session.rb
+++ lib/puppet/type/consul_session.rb
@@ -12,7 +12,6 @@ Puppet::Type.newtype(:consul_session) do
   newparam(:node) do
     desc 'Name of the node for this session'
     defaultto Facter.value(:hostname)
-    #defaultto 'derp'
   end
 
   newparam(:lockdelay) do
@@ -23,21 +22,8 @@ Puppet::Type.newtype(:consul_session) do
     end
   end
 
-  newparam(:checks) do
+  newparam(:checks, :array_matching => :all) do
     desc 'List of checks. Includes serfhealth by default'
     defaultto ['serfHealth']
-  end
-
-  newparam(:url) do
-    desc 'Consul url to use'
-    defaultto 'http://localhost:8500/v1/session'
-  end
-
-  newparam(:tries) do
-    desc 'The amount of times to retry before failing'
-    defaultto 1
-    munge do |v|
-      Integer(v)
-    end
   end
 end
