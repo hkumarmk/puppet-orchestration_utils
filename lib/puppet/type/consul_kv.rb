a511726b0cc52943efda9141c51083df3910987e
hkumarmk <hkumarmk@gmail.com>
2015-02-24 11:27:25 +0530

    Changes in consul_kv
    * Use jiocloud::utils
    * Support all params for consul kv
      cas - check and set operation
      flags - set flags
      acquire - acquire a lock
      release - release a lock

--- lib/puppet/type/consul_kv.rb
+++ lib/puppet/type/consul_kv.rb
@@ -23,12 +23,47 @@ doesn't exist. If the key exists with different value, it will be changed.
     defaultto 'http://localhost:8500/v1/kv'
   end
 
+  newproperty(:acquire) do
+    desc 'Session name to acquire the lock'
+  end
+
+  newproperty(:release) do
+    desc 'Release existing session or not, It takes three possible values,
+      Yes - release any existing session lock
+      No  - It is same as not setting this property
+      <session name>  - Name of the session to be tried, if any other session is
+locked, this will be ignored and nothing will happen'
+    munge do |v|
+      if v.match(/(yes)|(no)/i)
+        v.capitalize
+      end
+    end
+  end
+
+  newparam(:node) do
+    desc 'optional node name to make sure only lock the session owned by a node'
+  end
+
+  newparam(:cas) do
+    desc 'convert put operations to check-and-set operations. The value is a
+      number, if the value is 0, consul will only put the keyif it does not already
+      exists, if its non-zero, then the key is only set if index matches modifyindex
+      of that key'
+    newvalues(/\d+/)
+  end
+
+  newparam(:flags) do
+    desc 'An arbitrary number which can be used by applications'
+    newvalues(/\d+/)
+  end
+
   newproperty(:value) do
     desc 'Value to set'
   end
 
   validate do
     raise(Puppet::Error, 'Value should be set') unless self[:value]
+    raise(Puppet::Error, 'Both acquire and release cannot be specified at a time') if self[:acquire] && self[:release]
   end
 
 end
