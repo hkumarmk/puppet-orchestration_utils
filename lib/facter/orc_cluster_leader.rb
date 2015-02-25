d61dcb0313e9d49994faa25e33e0434884334757
hkumarmk <hkumarmk@gmail.com>
2015-02-25 11:24:46 +0530

    Avoid handling exception in utils, do it whenever required
    * Removed exception handling on utils
    * Handle the exception in facter code to avoid facter to kill puppet run forever
     in case any issue on consul or consul is not setup as puppet shall have code
     to fix or setup consul.
    * Added appropriate inline doc
    * fixed some small misses and changes

--- lib/facter/orc_cluster_leader.rb
+++ lib/facter/orc_cluster_leader.rb
@@ -5,29 +5,60 @@ require 'jiocloud/utils'
 include Jiocloud::Utils
 
 hostname = Facter.value(:hostname)
-sessions = getNodeSessionNames(hostname)
+
+##
+# getNodeSessionNames may fail if consul is not setup or if consul is down, and
+# this failure should not fail the facter as puppet shall have code to
+# setup/correct consul.
+##
+begin
+  sessions = getNodeSessionNames(hostname)
+rescue
+  true
+end
+
+##
+# Sessions can be nil if consul is down or not setup currently (may be on first puppet
+# run)
+##
 if ! sessions.nil?
+
+  ##
+  # We only care about sessions on specific format - we expect the valid session
+  # in form <servicename>~<hostname>
+  ##
   valid_sessions = sessions.select { |session| /~/.match(session) }
 
+  ##
+  # Iterate through all valid sessions and try to acquire a key,
+  ##
   valid_sessions.each do |session|
     service,host = session.split('~')
     key =  "clusters/#{service}/leader"
-    createKV(key,hostname,{:acquire => session})
+
+    ##
+    # leader is set if it is able to acquire the lock.
+    # If somebody else acquire the lock, then set leader_name from the kv, else
+    # set it as hostname.
+    ##
     leader = true if createKV(key,hostname,{:acquire => session}) == true || (leader_name = getKvValue(key)) == hostname
 
+    ##
+    # This fact can be used in the puppet code to get the leader node name.
+    ##
     Facter.add(:leader_node) do
       setcode do
-        if leader
-          hostname
-        else
-          leader_name
-        end
+        leader_name
       end
     end
 
+    ##
+    # This fact can be used to build hiera hierarcy and it give a ready to use
+    # fact for other puppet code for the node's role in the cluster.
+    ##
     Facter.add(:cluster_role) do
       setcode do
-        if leader
+        if leader_name == hostname
           'leader'
         else
           'follower'
